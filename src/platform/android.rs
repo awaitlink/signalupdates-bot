2fb617b34b0ec9cb8cc9a9610f605e5aa760d619
Artem Varaksa <artvaraksa@gmail.com>
2023-06-03 21:58:49 +0300

    Fix clippy warnings.

--- src/platform/android.rs
+++ src/platform/android.rs
@@ -15,12 +15,12 @@ impl BuildConfiguration {
         // Canonical version code
 
         let canonical_version_code =
-            Self::get_u64(&text, r"def\s+canonicalVersionCode\s*=\s*(\d+)", 1)
+            Self::get_u64(text, r"def\s+canonicalVersionCode\s*=\s*(\d+)", 1)
                 .context("couldn't get canonical_version_code")?;
 
         // Postfix size
 
-        let postfix_size = Self::get_u64(&text, r"def\s+postFixSize\s*=\s*(\d+)", 1)
+        let postfix_size = Self::get_u64(text, r"def\s+postFixSize\s*=\s*(\d+)", 1)
             .context("couldn't get postfix_size")?;
 
         // ABI postfixes
@@ -29,10 +29,10 @@ impl BuildConfiguration {
             .context("couldn't compile abi_postfixes_regex")?;
 
         let caps = abi_postfixes_regex
-            .captures(&text)
+            .captures(text)
             .context("couldn't find abi postfixes in file")?;
 
-        let json = (String::from("{") + &caps[1] + "}").replace("'", "\"");
+        let json = (String::from("{") + &caps[1] + "}").replace('\'', "\"");
 
         let abi_postfixes: HashMap<String, u64> =
             serde_json::from_str(&json).context("couldn't parse abi postfixes")?;
@@ -46,7 +46,7 @@ impl BuildConfiguration {
 
     fn get_u64(text: &str, regex: &str, capture_number: usize) -> anyhow::Result<u64> {
         let re = Regex::new(regex).context("couldn't compile regex")?;
-        let caps = re.captures(&text).context("couldn't find match in text")?;
+        let caps = re.captures(text).context("couldn't find match in text")?;
         str::parse(&caps[capture_number]).context("couldn't parse as u64")
     }
 }
